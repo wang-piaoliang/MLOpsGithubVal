b9425c5b256eac4d65ca4f89ad235392fe08e059
王脸小 <52517818+wang-piaoliang@users.noreply.github.com>
2021-06-17 11:41:10 +0800

    Update train.py

--- code/train/train.py
+++ code/train/train.py
@@ -16,22 +16,6 @@ print("submitting pipeline aml-run-val")
 pipeline_run = published_pipeline.submit("aml-run-val")
 print("pipeline aml-run-val run completed")
 
-    
-"""
-svc_pr_password = "QOFpAEY8.0o7~_Z61cX7YkPFnw~_-M_QcI"
-svc_pr = ServicePrincipalAuthentication(
-    tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47",
-    service_principal_id="b5d973af-dab6-44d1-9141-cb325843ffd2",
-    service_principal_password=svc_pr_password)
-
-ws = Workspace(
-    subscription_id="2acfc131-8c35-4698-95d8-df43d793ca8b",
-    resource_group="MLOpsGithub",
-    workspace_name="demoAzMLWorkspace",
-    auth=svc_pr
-    )
-"""
-
 
 
 
